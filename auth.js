be216771d9fa190ff2c4df1c7f95060059297f8d
Alereza_RaDBlaCK01 <aliireza01ahmadi@gmail.com>
2024-02-22 18:32:24 +0100

    getting data token and save it in cookie

---
+++ auth.js
@@ -0,0 +1,21 @@
+import { postData } from "./utils/httpReq.js";
+
+const inputsBox = document.querySelectorAll("input");
+const loginButton = document.querySelector("button");
+
+
+const submitHandler = async (event) => {
+    event.preventDefault();
+
+    const username = inputsBox[0].value;
+    const password = inputsBox[1].value;
+
+
+    const response = await postData("auth/login", {
+        username,
+        password,
+    });
+    document.cookie = `token=${response.token}; max-age=${24 * 60 * 60}`
+};
+
+loginButton.addEventListener("click", submitHandler);
