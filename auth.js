20d2611fb2fdd4b2c6722dfe62920385d8fbe7c8
Alereza_RaDBlaCK01 <aliireza01ahmadi@gmail.com>
2024-02-23 13:30:54 +0100

    making authhandler

--- auth.js
+++ auth.js
@@ -1,5 +1,6 @@
 import { postData } from "./utils/httpReq.js";
-import { getCookie, setCookie } from "./utils/cookie.js";
+import { setCookie } from "./utils/cookie.js";
+import authHandler from "./utils/authorization.js";
 
 const inputsBox = document.querySelectorAll("input");
 const loginButton = document.querySelector("button");
@@ -20,12 +21,7 @@ const submitHandler = async (event) => {
     location.assign("index.html");
 };
 
-const init = () => {
-    const cookie = getCookie();
-    if (cookie) {
-        location.assign("index.html");
-    }
-}
+ 
 
 loginButton.addEventListener("click", submitHandler);
-document.addEventListener("DOMContentLoaded", init);
+document.addEventListener("DOMContentLoaded", authHandler);
