40d86c51ff1a76dba8c0640c2f26f9a153499578
Alereza_RaDBlaCK01 <aliireza01ahmadi@gmail.com>
2024-02-27 23:02:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,23 @@ import { getData } from "./utils/httpReq.js";
 
 const loginButton = document.getElementById("login");
 const dashboardButton = document.getElementById("dashboard");
+const mainContent = document.getElementById("products")
 
-const showProducts = () => {}
+const showProducts = (products) => {
+    mainContent.innerHTML = "";
+
+    products.forEach((product) => {
+        const JSX = `
+        <div>
+        <img alt=${product.title} src=${product.image} />
+        <h4>${product.title}</h4>
+        </div>
+        `;
+
+        mainContent.innerHTML += JSX;
+    });
+
+}
 
 const init = async () => {
     const cookie = getCookie();
